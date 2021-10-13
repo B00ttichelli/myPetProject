549e09b90e2df3068b585063f48e93532d114574
B00ttichelli <donttrytofish@protonmail.com>
2021-10-13 04:34:42 +0300

    User Registration methods added

--- src/main/java/com/vovnenko/mypetproject/model/User.java
+++ src/main/java/com/vovnenko/mypetproject/model/User.java
@@ -4,14 +4,14 @@ package com.vovnenko.mypetproject.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UniqueElements;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
+@Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,10 +21,12 @@ public class User {
     private Long userId;
 
     @NotBlank(message = "Username cant be blank")
+    @Column(unique = true)
     private String username;
 
     @NotBlank(message = "Password is required field")
     private String password;
+
 
     private Instant created;
 
---
+++ src/main/java/com/vovnenko/mypetproject/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.vovnenko.mypetproject.service;
+
+import com.vovnenko.mypetproject.dto.RegisterRequest;
+import com.vovnenko.mypetproject.model.User;
+import com.vovnenko.mypetproject.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(true);
+
+        userRepository.save(user);
+    }
+}
