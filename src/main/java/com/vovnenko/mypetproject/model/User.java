f9708fa61955989d3efa7a79d257349f3cf19c52
B00ttichelli <donttrytofish@protonmail.com>
2021-10-18 23:30:34 +0300

    User Registration methods added

--- src/main/java/com/vovnenko/mypetproject/model/User.java
+++ src/main/java/com/vovnenko/mypetproject/model/User.java
@@ -4,10 +4,8 @@ package com.vovnenko.mypetproject.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
