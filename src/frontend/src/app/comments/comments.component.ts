33f0cefe57749e90d8bb358f11471ff938a294fc
B00ttichelli <donttrytofish@protonmail.com>
2021-12-21 19:58:34 +0200

    Frontend:  Form for Comments Added

--- src/frontend/src/app/comments/comments.component.ts
+++ src/frontend/src/app/comments/comments.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {CommentModel} from "../../shared/commentModel";
 import {PostsService} from "../service/posts.service";
 import {CommentService} from "../service/comment.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {PostModel} from "../../shared/postModel";
+import {CommentPayload} from "../../shared/commentPayload";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-comments',
@@ -11,25 +13,51 @@ import {PostModel} from "../../shared/postModel";
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
+  createCommentForm: FormGroup;
+  commentPayload: CommentPayload;
   post: PostModel;
   comments: Array<CommentModel> = [];
+
+
+
+
 
   constructor(private postsService:PostsService,
               private commentsService:CommentService,
               private route:ActivatedRoute,
+              private router:Router
               ) {
 
-
+    this.commentPayload = {
+      postId:0,
+      text:''
+    }
 
     this.route.params.subscribe(params=>{
+      this.commentPayload.postId = params['id'];
       this.commentsService.getAllCommentsByPostId(params['id'])
         .subscribe(data=>{this.comments = data});
       this.postsService.getPostById(params['id']).subscribe(post=>{this.post = post});
+
     })
   }
 
   ngOnInit(): void {
+      this.createCommentForm = new FormGroup({
+        postBody:new FormControl('',Validators.required)
+      })
+  }
+
+  createComment(){
+
+    this.commentPayload.text = this.createCommentForm.get('postBody').value;
+
+    this.commentsService.postComment(this.commentPayload).subscribe()
+
+    this.router.navigateByUrl('/')
 
   }
+  discardComment(){}
+
 
 }
--- src/frontend/src/app/service/comment.service.ts
+++ src/frontend/src/app/service/comment.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {CommentModel} from "../../shared/commentModel";
+import {CommentPayload} from "../../shared/commentPayload";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,8 @@ export class CommentService {
   getAllCommentsByPostId(id):Observable<Array<CommentModel>>{
     return this.http.get<Array<CommentModel>>("http://localhost:8080/api/comment/get?id="+id);
 
+  }
+  postComment(commentPayload:CommentPayload){
+    return this.http.post<CommentModel>("http://localhost:8080/api/comment/create",commentPayload);
   }
 }
